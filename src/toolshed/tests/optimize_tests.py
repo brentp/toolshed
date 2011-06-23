fcfb77ab74a4c07da77bcfb910d9f5b97cc0bd06
Brent Pedersen <bpederse@gmail.com>
2011-06-23 09:56:42 -0600

    move the function to the toplevel since it can't handle indents currently.

--- src/toolshed/tests/optimize_tests.py
+++ src/toolshed/tests/optimize_tests.py
@@ -1,28 +1,32 @@
-from toolshed import shedskinner
-
 def adder(a, b):
     return a + b
 
-adder_ss = shedskinner((4, 5))(adder)
+try:
+    from toolshed import shedskinner
 
 
-def test_adder():
-    assert adder(2, 3)  == 5
+    adder_ss = shedskinner((4, 5))(adder)
 
-def test_adder_ss():
-    assert adder_ss(2, 3)  == 5
+    def test_adder():
+        assert adder(2, 3)  == 5
 
-def teardown_func():
-    import os
-    import glob
-    for f in glob.glob("shedskin*.py*"):
-        os.unlink(f)
-    for f in glob.glob("shedskin*.*pp"):
-        os.unlink(f)
-    for f in glob.glob("shedskin*.so"):
-        os.unlink(f)
-    for f in glob.glob("Makefile_*"):
-        if len(f) > 20:
+    def test_adder_ss():
+        assert adder_ss(2, 3)  == 5
+
+    def teardown_func():
+        import os
+        import glob
+        for f in glob.glob("shedskin*.py*"):
             os.unlink(f)
+        for f in glob.glob("shedskin*.*pp"):
+            os.unlink(f)
+        for f in glob.glob("shedskin*.so"):
+            os.unlink(f)
+        for f in glob.glob("Makefile_*"):
+            if len(f) > 20:
+                os.unlink(f)
 
-test_adder_ss.teardown = teardown_func
+    test_adder_ss.teardown = teardown_func
+
+except ImportError:
+    pass
