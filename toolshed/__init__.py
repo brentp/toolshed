3c1b11616c5a5990f1924619263f7e9c8d8120f9
Brent Pedersen <bpederse@gmail.com>
2013-08-20 16:39:11 -0600

    add groupby

--- toolshed/__init__.py
+++ toolshed/__init__.py
@@ -7,6 +7,25 @@ try:
 except ImportError:
     pass
 
+from itertools import groupby as igroupby
+from operator import itemgetter
+
+def groupby(iterable, key=0, filter=None):
+    """
+    wrapper to itertools.groupby that
+    1) returns a list of each group, rather than a generator
+    2) accepts integers or strings as the key and automatically
+       converts them to callables with itemgetter(key)
+
+    e.g. groupby(iterable, 0)
+    """
+    if isinstance(key, (basestring, int)):
+        key = itemgetter(key)
+    elif isinstance(key, (tuple, list)):
+        key = itemgetter(*key)
+    for label, grp in igroupby(iterable, key):
+        yield label, list(grp)
+
 
 def main():
     #import argparse
---
+++ toolshed/tests/group_test.py
@@ -0,0 +1,22 @@
+from toolshed import groupby
+import os.path as op
+import sys
+from operator import itemgetter
+
+def test_group():
+    data = zip([1, 1, 1, 2, 2, 2], ['a', 'a', 'b', 'c', 'd', 'f'])
+    print >>sys.stderr, data
+
+
+    res = list(groupby(data, (0, 1)))
+    for key, grp in res:
+        assert isinstance(grp, list)
+        assert len(set(grp)) == 1
+
+
+    res1 = list(groupby(data, 0))
+    res2 = list(groupby(data, itemgetter(0)))
+    assert res1 == res2
+
+
+
