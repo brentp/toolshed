670c4b0abce8e5168909814a73fe1b3186c198cd
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-03-21 07:29:37 -0600

    allow pmap to be used on instance methods

--- toolshed/pool.py
+++ toolshed/pool.py
@@ -37,6 +37,29 @@ else:
     IMapIterator.__next__ = wrapper(IMapIterator.__next__)
 
 
+# allow instance methods to be pickled.
+# from Steven Bethard
+# http://stackoverflow.com/questions/1816958/cant-pickle-type-instancemethod-when-using-pythons-multiprocessing-pool-ma
+from copy_reg import pickle
+from types import MethodType
+
+def _pickle_method(method):
+    func_name = method.im_func.__name__
+    obj = method.im_self
+    cls = method.im_class
+    return _unpickle_method, (func_name, obj, cls)
+
+def _unpickle_method(func_name, obj, cls):
+    for cls in cls.mro():
+        try:
+            func = cls.__dict__[func_name]
+        except KeyError:
+            pass
+        else:
+            break
+    return func.__get__(obj, cls)
+
+pickle(MethodType, _pickle_method, _unpickle_method)
 
 #def pool_sig():
 #    signal.signal(signal.SIGINT, signal.SIG_IGN)
